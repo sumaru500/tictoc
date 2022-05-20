21630a043d57f46fb840a24c688ffd2ec2a504db
sumaru500 <phamthang37@yahoo.com>
2022-05-20 15:02:52 +0200

    MAJ lesson 60

--- src/App.js
+++ src/App.js
@@ -1,37 +1,35 @@
 import { Routes, Route, Link } from 'react-router-dom';
-import Home from './pages/Home'
-import News from './pages/News'
+import Home from './pages/Home';
+import News from './pages/News';
 import Contact from './pages/Contact';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/news">News</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-        </ul>
-      </nav>
+    return (
+        <div className="App">
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/news">News</Link>
+                    </li>
+                    <li>
+                        <Link to="/contact">Contact</Link>
+                    </li>
+                </ul>
+            </nav>
 
+            {/* Router */}
 
-      {/* Router */}
-
-      <Routes>
-        <Route path="/news" element={<News/>}/>
-        <Route path="/contact" element={<Contact/>}/>
-        <Route path="/" element={<Home/>}/>
-      </Routes>
-
-    </div>
-  );
+            <Routes>
+                <Route path="/news" element={<News />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
