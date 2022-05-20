caf3475f420640de2ce7729ec9f7e2328cc6661e
sumaru500 <phamthang37@yahoo.com>
2022-05-20 15:00:17 +0200

    Until lesson 60 Prettier

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import { Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home'
+import News from './pages/News'
+import Contact from './pages/Contact';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/news">News</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+
+
+      {/* Router */}
+
+      <Routes>
+        <Route path="/news" element={<News/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+        <Route path="/" element={<Home/>}/>
+      </Routes>
+
     </div>
   );
 }
