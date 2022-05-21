37597c1680a6bc5682a1a5c1fe35ccea3f1e73a4
sumaru500 <phamthang37@yahoo.com>
2022-05-21 11:01:13 +0200

    lesson 64 CSS Popper dropdown menu

---
+++ src/components/AccountItem/index.js
@@ -0,0 +1,27 @@
+import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+
+import styles from './AccountItem.module.scss';
+
+const cx = classNames.bind(styles);
+function AccountItem() {
+    return (
+        <div className={cx('wrapper')}>
+            <img
+                className={cx('avatar')}
+                src="https://p77-sign-va.tiktokcdn.com/tos-maliva-avt-0068/aa2d64061c9886a0b9efa32975c6b32d~c5_300x300.webp?x-expires=1653260400&x-signature=iEBbMM50NF0CitFtOhqBmQ8niSI%3D"
+                alt="avatar"
+            ></img>
+            <div className={cx('info')}>
+                <h4 className={cx('name')}>
+                    <span>Nguyen Van A</span>
+                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                </h4>
+                <span className={cx('username')}>nguyenvana</span>
+            </div>
+        </div>
+    );
+}
+
+export default AccountItem;
